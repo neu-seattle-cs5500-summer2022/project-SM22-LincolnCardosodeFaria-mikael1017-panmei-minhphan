6fe1d210d915e1a0b9078729c53cad798256f8be
Sistema Pesquisa <sistema_pesquisa@outlook.com>
2022-06-11 10:02:39 -0700

    add backend v2

---
+++ backend/CS5500_Final/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using CS5500_Final.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace CS5500_Final.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        [ActivatorUtilitiesConstructor]
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost(nameof(Create))]
+        public async Task<int> Create(User data)
+        {
+            int resultId = 99999;
+            string myDb1ConnectionString = _configuration.GetConnectionString("DefaultConnection");
+            try
+            {
+                using (var connection = new SqlConnection(myDb1ConnectionString))
+                {
+                    await connection.OpenAsync();
+                    var sqlStatement = @"
+                                    INSERT INTO Users 
+                                    (Name
+                                    ,Type
+                                    ,Password    
+                                    )
+                                    VALUES (@Name
+                                    ,@Type
+                                    ,@Password
+                                    )";
+
+                    resultId = await connection.ExecuteScalarAsync<int>(sqlStatement, data);
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+
+            return resultId;
+        }
+    }
+}
