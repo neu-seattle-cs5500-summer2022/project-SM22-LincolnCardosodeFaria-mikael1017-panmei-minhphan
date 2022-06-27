95276edda0e998b052800759177bb8119c924416
Jaewoo <jaewoocho1017@gmail.com>
2022-06-26 18:09:19 -0700

    added navbar and homepage

--- frontend/frontend/src/containers/HomePage.js
+++ frontend/frontend/src/containers/HomePage.js
@@ -1,28 +1,31 @@
 import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import cardImage from "../homepage_card.jpeg";
+import MyNavbar from "../components/Navbar.js";
 
-function Homepage() {
+export default function Homepage() {
   const navigate = useNavigate();
   const goSignUp = () => {
     navigate("/signup");
   };
 
-  const goLogin = () => {
-    navigate("/login");
-  };
-
   return (
     <div>
-      <Button variant="primary" onClick={goSignUp}>
-        SignUp
-      </Button>
-      <Button variant="primary" onClick={goLogin}>
-        Login
-      </Button>
+      <MyNavbar></MyNavbar>
+      <header className="App-header">
+        <Card className="m-5 border-0" style={{ color: "#000" }}>
+          <Card.Img src={cardImage} />
+          <Card.Body>
+            <Card.Title>Gym management app</Card.Title>
+            <Card.Text>This is an description of an app</Card.Text>
+            <Button variant="primary" onClick={goSignUp}>
+              SignUp
+            </Button>
+          </Card.Body>
+        </Card>
+      </header>
     </div>
   );
 }
-
-export default Homepage;
