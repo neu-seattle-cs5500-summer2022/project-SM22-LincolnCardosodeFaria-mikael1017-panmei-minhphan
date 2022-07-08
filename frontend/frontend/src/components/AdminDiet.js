20c269181ce9c519fcdf0a787244b9dd30197f1d
Jaewoo <jaewoocho1017@gmail.com>
2022-07-07 20:09:51 -0700

    fixed values for weekDay

--- frontend/frontend/src/components/AdminDiet.js
+++ frontend/frontend/src/components/AdminDiet.js
@@ -64,13 +64,13 @@ function AdminDiet(props) {
               value={weekDay}
               onChange={(e) => setWeekDay(e.target.value)}
             >
-              <option value={0}> Monday </option>
-              <option value={1}> Tuesday </option>
-              <option value={2}> Wednesday </option>
-              <option value={3}> Thursday </option>
-              <option value={4}> Friday</option>
-              <option value={5}> Saturday</option>
-              <option value={6}> Sunday</option>
+              <option value={1}> Monday </option>
+              <option value={2}> Tuesday </option>
+              <option value={3}> Wednesday </option>
+              <option value={4}> Thursday </option>
+              <option value={5}> Friday</option>
+              <option value={6}> Saturday</option>
+              <option value={0}> Sunday</option>
             </Form.Control>
           </Form.Group>
           <div>
