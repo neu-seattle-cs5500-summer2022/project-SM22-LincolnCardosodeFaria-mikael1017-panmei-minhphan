ad09f281da0d4dcd473278a1ed5dd8c5fea62bd1
Sistema Pesquisa <sistema_pesquisa@outlook.com>
2022-08-08 16:31:11 -0700

    Add Test

---
+++ CS5500_Final/UnitTests/UsersTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+namespace CS5500_Final.UnitTests
+{
+
+    public class UsersTests
+    {
+
+        [Fact]
+        public void IsInvalideUser_InputInvalide_ReturnFalse()
+        {
+
+           Assert.False(false);
+        }
+    }
+}
