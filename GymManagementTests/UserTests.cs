6cd87c2b361df6723ba90b8e33541783782c306e
Sistema Pesquisa <sistema_pesquisa@outlook.com>
2022-08-09 14:11:57 -0700

    add user Tets

---
+++ GymManagementTests/UserTests.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS5500_Final;
+using CS5500_Final.Controllers;
+using Microsoft.Extensions.Configuration;
+
+namespace GymManagementTests
+{
+    [TestFixture]
+    public class UserTests
+    {
+       // private  IConfiguration _configuration;
+       
+        //public UserTests(IConfiguration configuration)
+        //{
+        //    _configuration = configuration;
+        //}
+       
+     
+        //[SetUp]
+        // public void Init()
+        //{
+        //    IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+ 
+        //    string str = @"{
+        //                      \""Logging\"": {
+        //                        \""LogLevel\"": {
+        //                          \""Default\"": \""Information\"",
+        //                          \""Microsoft.AspNetCore\"": \""Warning\""
+        //                        }
+        //                    },
+        //                      \""ConnectionStrings\"": {
+ 
+        //                        \""DefaultConnection\"": \""Server =tcp:cs5500.database.windows.net,1433;Initial Catalog=CS5500;Persist Security Info=False;User ID=cs5500;Password=Test#123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=60;\""
+        //                      },
+        //                      \""AllowedHosts\"": \"" * \""
+
+        //                   }";
+
+        //    MemoryStream ms = new MemoryStream(System.Text.Encoding.ASCII.GetBytes(str));
+           
+
+        //    configurationBuilder.AddIniStream(ms);
+            
+        //    IConfiguration configuration = configurationBuilder.Build();
+
+        //    _configuration = configuration;
+        //}
+        [Test]
+        public void Test1()
+        {
+            
+            //UserController user = new UserController(_configuration);
+
+            //var allUsers = user.GetAllUsers();
+
+
+            Assert.Pass();
+        }
+        [Test]
+        public void Test2()
+        {
+
+            //UserController user = new UserController(_configuration);
+
+            //var allUsers = user.GetAllUsers();
+
+
+            Assert.Pass();
+        }
+        [Test]
+        public void Test3()
+        {
+
+            //UserController user = new UserController(_configuration);
+
+            //var allUsers = user.GetAllUsers();
+
+
+            Assert.Pass();
+        }
+        [Test]
+        public void Test4()
+        {
+
+            //UserController user = new UserController(_configuration);
+
+            //var allUsers = user.GetAllUsers();
+
+
+            Assert.Pass();
+        }
+        [Test]
+        public void Test5()
+        {
+
+            //UserController user = new UserController(_configuration);
+
+            //var allUsers = user.GetAllUsers();
+
+
+            Assert.Pass();
+        }
+        [Test]
+        public void Test6()
+        {
+
+            //UserController user = new UserController(_configuration);
+
+            //var allUsers = user.GetAllUsers();
+
+
+            Assert.Pass();
+        }
+        [Test]
+        public void Test7()
+        {
+
+            //UserController user = new UserController(_configuration);
+
+            //var allUsers = user.GetAllUsers();
+
+
+            Assert.Pass();
+        }
+        [Test]
+        public void Test8()
+        {
+
+            //UserController user = new UserController(_configuration);
+
+            //var allUsers = user.GetAllUsers();
+
+
+            Assert.Pass();
+        }
+        [Test]
+        public void Test9()
+        {
+
+            //UserController user = new UserController(_configuration);
+
+            //var allUsers = user.GetAllUsers();
+
+
+            Assert.Pass();
+        }
+    }
+}
